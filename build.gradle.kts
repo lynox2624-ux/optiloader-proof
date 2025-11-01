c2dadbf3b0c4e3797f91127956895bc3888d8c05
lynox2624-ux <lynox2624@gmail.com>
2025-11-01 13:33:47 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,29 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
+    id("java")
 }
+
+group = "com.optloader"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.jar {
+    archiveBaseName.set("OptiLoader-Proof")
+    archiveVersion.set("1.0.0")
+    from(sourceSets.main.get().output)
+    manifest {
+        attributes["Main-Class"] = "com.optloader.core.OptiLoaderCore"
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
