3a3c14dd809f035ad03dd8bf3a47e62aecc56e26
lynox2624-ux <lynox2624@gmail.com>
2025-11-01 10:27:09 +0530

    Create OptiLoader.kt

---
+++ src/main/kotlin/com/lynox/optiloader/OptiLoader.kt
@@ -0,0 +1,9 @@
+package com.lynox.optiloader
+
+object OptiLoader {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("OptiLoader initialized successfully for 1.21.4!")
+        println("This is a proof-of-concept bootstrap. Place mod jars in the mods/ folder with manifest key OptiLoader-Entrypoint.")
+    }
+}
